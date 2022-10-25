e50fea4b9f3671809a9b8d83f0ce01dc687480bf
James <James.reinhardt1@gmail.com>
2022-10-24 21:40:28 -0500

    Updated changes to HP project

--- main.js
+++ main.js
@@ -1,1 +1,60 @@
-console.log("Yoooo");
+const wizardsArray = [
+  {
+  id: 1,
+  name: 'Big Jim',
+  house: 'Huffle Puff',
+  },
+  {
+  id: 2,
+  name: 'Alyssa',
+  house: 'Huffle Puff',
+  },
+  {
+  id: 3,
+  name: 'Gus',
+  house: 'Gryffindor',
+  },
+  {
+  id: 4,
+  name: 'Leia',
+  house: 'Ravenclaw',
+  }
+];
+
+const voldemortArray = [
+  {
+    id: 1,
+    name: 'Mario',
+    expelled: 'expelled'
+  },
+  {
+    id: 2,
+    name: ''
+  }
+]
+
+const openingPageDiv = document.querySelector('#openingPage');
+const  mainPageDiv = document.querySelector('#mainPage');
+const schoolsDiv = document.querySelector ('#schools');
+const studentCardsDiv = document.querySelector('#studentCards');
+const goodCardsDiv = document.querySelector('#goodCards');
+const badCardsDiv = document.querySelector ('#badCards');
+
+
+const renderToDom = (divId, textToRender) => {
+  const selectedElement = document.querySelector(divId);
+  selectedElement.innerHTML = textToRender;
+};
+
+const startingPage = () => {
+  const bootstrapCardString = `<div class="firstCard" style="width: 18rem;">
+  <img src="https://aux2.iconspalace.com/uploads/447401561.png" class="card-img-top" alt="Sorting Hat">
+  <div class="firstCard-body">
+    <h5 class="talkingSortingHat">Welcome! Lets make you a wizard!</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+}
+
+const 
